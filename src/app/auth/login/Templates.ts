1727a55a72251a9f16536eada0d8ea7a7ce1a059
Ant <hello31089@ya.ru>
2025-02-14 20:09:37 +0300

    First commit

---
+++ src/app/auth/login/Templates.ts
@@ -0,0 +1,7 @@
+
+export const Templates={
+   IS_ERROR_EMAIL_TEXT:'Введите электронную почту пользователя',
+   IS_ERROR_PASSWORD_TEXT:'Введите пароль пользователя',
+   EMAIL_TEXT:'',
+   PASSWORD_TEXT:'',
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <AntdRegistry>
         {children}
+      </AntdRegistry>
       </body>
     </html>
   );
